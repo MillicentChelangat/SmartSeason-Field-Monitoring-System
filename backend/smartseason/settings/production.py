bb9323a6c889738bc56d4a89b4690fabdc4a5c87
MillicentChelangat <millicentchelangat42@gmail.com>
2026-06-16 13:48:06 +0300

    new changes

--- backend/smartseason/settings/production.py
+++ backend/smartseason/settings/production.py
@@ -2,8 +2,8 @@ from .base import *
 import os
 import dj_database_url
 
-#DEBUG = False
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = False
+#DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
     'smartseason-field-monitoring-system-1-bm87.onrender.com',
